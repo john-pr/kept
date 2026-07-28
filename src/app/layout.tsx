826a38594f51b0c66d000a36b2c8c11b61f6d86c
jan p <janprzybysz2@gmail.com>
2026-07-28 18:34:30 +0200

    test redeploy commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+
         <Toaster position="top-center" />
       </body>
     </html>
